635e504de4d4d9f45e4f03dfc8cdb98091397bc4
Eisiehame <knurov24@gmail.com>
2023-03-06 22:20:40 +0200

    lesson 15.3-2 - bee

--- linked_list.py
+++ linked_list.py
@@ -1,22 +1,58 @@
 
 
 class Node:
-    def __init__(self, data:dict, next_node = None):
+    def __init__(self, data, next_node = None):
         self.data = data  #  данные
         self.next_node = next_node  # ссылка на следующий
 
 class LinkedList:
-    def __init__(self, head=None, tail=None):
-        self.head = Node(head)
-        self.tail = Node(tail)
-
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.sum_of_items = 0
     def insert_beginning(self, data):
-        self.head.next_node = self.head
-        self.head = Node(data)
+        self.sum_of_items += 1
+        if self.head is None:
+            self.head = Node(data)
+
+        else:
+            if self.sum_of_items == 2:
+                x = self.head
+                self.head = Node(data)
+                self.head.next_node = x
+            elif self.sum_of_items == 3:
+                x = self.head
+                self.head = Node(data)
+                self.head.next_node = x
+                self.head.next_node.next_node = x.next_node
+
+
 
     def insert_at_end(self, data):
-        self.tail.next_node = Node(data)
-        self.tail = Node(data)
+        self.sum_of_items += 1
+
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            if self.sum_of_items == 1:
+                self.head.next_node = Node(data)
+                self.tail = Node(data)
+            # elif self.sum_of_items == 2:
+            #     self.head.next_node.next_node = Node(data)
+            #     self.tail = Node(data)
+            # elif self.sum_of_items == 3:
+            #     self.head.next_node.next_node.next_node = Node(data)
+            #     self.tail = Node(data)
+            # elif self.sum_of_items == 4:
+            #     self.head.next_node.next_node.next_node.next_node = Node(data)
+            #     self.tail = Node(data)
+            # elif self.sum_of_items == 5:
+            #     self.head.next_node.next_node.next_node.next_node.next_node = Node(data)
+            #     self.tail = Node(data)
+            #
+
+
+
 
     def print_ll(self):
         ll_string = ''
@@ -29,7 +65,7 @@ class LinkedList:
 
         ll_string += ' None'
         print(ll_string)
-
+        print(self.sum_of_items)
 
 ll = LinkedList()
 print(ll.head)
